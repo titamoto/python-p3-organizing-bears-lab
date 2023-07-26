6f600a94f60490010c66e2c8dea52b08fb189149
Aleksandr <titamoto@gmail.com>
2023-07-26 13:35:33 -0700

    complete lab

---
+++ lib/insert.sql
@@ -0,0 +1,23 @@
+INSERT INTO bears (name, age, sex, color, temperament, alive)
+VALUES ("Mr. Chocolate", 3, "M", "teal", "dick", 0);
+
+INSERT INTO bears (name, age, sex, color, temperament, alive)
+VALUES ("Rowdy", 125, "M", "pink", "gigolo", 1);
+
+INSERT INTO bears (name, age, sex, color, temperament, alive)
+VALUES ("Tabitha", 1, "F", "black", "slut", 0);
+
+INSERT INTO bears (name, age, sex, color, temperament, alive)
+VALUES ("Sergeant Brown", 55, "F", "not_brown", "nice", 1);
+
+INSERT INTO bears (name, age, sex, color, temperament, alive)
+VALUES ("Melissa", 1488, "F", "white", "tolerant", 1);
+
+INSERT INTO bears (name, age, sex, color, temperament, alive)
+VALUES ("Grinch", 17, "M", "yellow", "charming", 0);
+
+INSERT INTO bears (name, age, sex, color, temperament, alive)
+VALUES ("Wendy", 5, "M", "blue", "fast-foody", 1);
+
+INSERT INTO bears (name, age, sex, color, temperament, alive)
+VALUES (NULL, 69, "M", "colorless", "memy", 0);
